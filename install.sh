46d611359242faa6f05e46468e85541722befb2a
Dan Gillespie <daniel.gillespie0949@gmail.com>
2020-04-23 00:06:04 -0700

    vmango experimental code

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ fi
 
 echo "Starting..."
 mkdir -p /storage # placeholder for some apps. Admin should reconfigure if necessary.
+mkdir -p /usr/src # where we'll download some code for install
 
 echo "Disable firewall... (configure and enable after setting up your core services)"
 ufw disable
@@ -27,6 +28,12 @@ ntpdate -u time.google.com
 
 echo "Install misc tools"
 apt-get install -y htop iotop iftop net-tools nano tmux screen vim
+
+# Dependency of vmango
+echo "Installing Golang..."
+add-apt-repository ppa:longsleep/golang-backports
+apt update
+apt install -y golang-go
 
 echo "Install Cockpit..."
 add-apt-repository ppa:cockpit-project/cockpit
@@ -63,40 +70,22 @@ docker run \
   linuxserver/heimdall
 
 echo "Installing KVM and QEMU..."
-apt-get install -y qemu qemu-kvm libvirt-bin libvirt-clients libvirt-daemon-system bridge-utils virt-manager
+apt-get install -y libvirt-bin qemu-kvm qemu-system
 kvm-ok
 sleep 5
 
-echo "Installing Kimchi, Wok, and dependencies..."
-mkdir -p /etc/wok/ssl
-openssl dhparam -out /etc/wok/ssl/dhparam.pem 2048
-openssl req -nodes -newkey rsa:2048 -keyout /etc/wok/ssl/wok.key -out /etc/wok/ssl/wok.csr -subj "/C=US/ST=California/L=San Francisco/O=Wok/OU=IT Department/CN=wok.local"
-openssl x509 -signkey /etc/wok/ssl/wok.key -in /etc/wok/ssl/wok.csr -req -days 365 -out /etc/wok/ssl/wok.crt
+echo "Configure KVM..."
+virsh pool-define-as default dir --target /var/lib/libvirt/images/
+virsh pool-start default
+virsh pool-autostart default
 
-
-mkdir -p /etc/kimchi/ssl
-openssl req -nodes -newkey rsa:2048 -keyout /etc/kimchi/ssl/kimchi.key -out /etc/kimchi/ssl/kimchi.csr -subj "/C=US/ST=California/L=San Francisco/O=Kimchi/OU=IT Department/CN=kimchi.local"
-openssl x509 -signkey /etc/kimchi/ssl/kimchi.key -in /etc/kimchi/ssl/kimchi.csr -req -days 365 -out /etc/kimchi/ssl/kimchi.crt
-
-docker run --name kimchi \
-  -v /etc/passwd:/etc/passwd:ro \
-  -v /run:/run \
-  -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
-  -v /etc/group:/etc/group:ro \
-  -v /etc/shadow:/etc/shadow:ro \
-  -v /var/run/libvirt/libvirt-sock:/var/run/libvirt/libvirt-sock \
-  -v /var/lib/libvirt:/var/lib/libvirt \
-  -v /etc/libvirt:/etc/libvirt \
-  -v /storage:/storage \
-  -v /etc/wok/ssl/dhparams.pem:/etc/wok/dhparams.pem:ro \
-  -v /etc/wok/ssl/wok.crt:/etc/wok/wok-cert.pem:ro \
-  -v /etc/wok/ssl/wok.key:/etc/wok/wok-key.pem:ro \
-  -v /etc/kimchi/ssl/kimchi.crt:/etc/kimchi/kimchi-cert.pem:ro \
-  -v /etc/kimchi/ssl/kimchi.key:/etc/kimchi/kimchi-key.pem:ro \
-  -p 8001:8001 \
-  --user 0:0 \
-  --privileged \
-  dantheman213/kimchi:latest
+echo "Installing vMango...."
+git clone https://github.com/subuk/vmango /usr/src/vmango
+cd /usr/src/vmango
+./dockerbuild.sh ubuntu-1804 make deb
+dpkg -i $(find . -name "*.deb" | head -1)
+usermod -aG libvirtd root
+newgrp libvirtd
 
 echo "Install Deluge..."
 mkdir -p $HOME/Downloads
@@ -121,7 +110,7 @@ printf "\nIndividual Service Dashboards:\n"
 echo "Cockpit: https://localhost:9090"
 echo "Deluge: http://localhost:8112"
 echo "Handbrake: http://localhost:5800"
-echo "Kimchi: https://localhost:8001"
+echo "Vmango: http://localhost:8080"
 echo "Portainer: http://localhost:9000"
 
 printf "\nCOMPLETE! Rebooting in 15 seconds....\n\n"
